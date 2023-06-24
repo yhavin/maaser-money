854ebe0b882edd61c6e34fd74a0e417bccb21a5f
yhavin <y.havin@gmail.com>
2023-06-25 01:14:43 +1000

    Add balance calculation

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref } from "vue"
+  import { ref, computed } from "vue"
 
 
   let incomeId = 0
@@ -30,69 +30,92 @@
     newMaaser.value = { description: "", amount: null, date: null }
   }
 
+  const totalIncome = computed(() => {
+    return incomes.value.reduce((sum, income) =>  sum + income.amount, 0)
+  })
+
+  const totalMaaser = computed(() => {
+    return maasers.value.reduce((sum, maaser) =>  sum + maaser.amount, 0)
+  })
+
+  const maaserDue = computed(() => {
+    return (totalIncome.value / 10) - totalMaaser.value
+  })
+
 </script>
 
 <template>
   <main class="container">
     <article>
-      <h3>Add income</h3>
-      <form @submit.prevent="handleSubmitIncome">
-        <div class="grid">
-          <input v-model="newIncome.description" placeholder="Income">
-          <input v-model.number="newIncome.amount" placeholder="Amount">
-        </div>
-        <button>Add income</button>
-      </form>
+      <div class="grid">
+        <form @submit.prevent="handleSubmitIncome">
+          <h3>Add income</h3>
+          <div class="grid">
+            <input v-model="newIncome.description" placeholder="Income">
+            <input v-model.number="newIncome.amount" placeholder="Amount">
+          </div>
+          <button>Add income</button>
+        </form>
 
-      <h3>Add ma'aser</h3>
-      <form @submit.prevent="handleSubmitMaaser">
-        <div class="grid">
-          <input v-model="newMaaser.description" placeholder="Income">
-          <input v-model.number="newMaaser.amount" placeholder="Amount">
-        </div>
-        <button>Add ma'aser</button>
-      </form>
+        <form @submit.prevent="handleSubmitMaaser">
+          <h3>Add ma'aser</h3>
+          <div class="grid">
+            <input v-model="newMaaser.description" placeholder="Ma'aser">
+            <input v-model.number="newMaaser.amount" placeholder="Amount">
+          </div>
+          <button>Add ma'aser</button>
+        </form>
+      </div>
     </article>
 
-    <details open>
-      <summary>Income</summary>
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(income, index) in incomes" :key="index">
-            <td>{{ income.description }}</td>
-            <td>{{ income.amount }}</td>
-            <td>{{ income.date.toLocaleDateString() }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </details>
+    <article>
+      <details open>
+        <summary>Income</summary>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(income, index) in incomes" :key="index">
+              <td>{{ income.description }}</td>
+              <td>{{ income.amount }}</td>
+              <td>{{ income.date.toLocaleDateString() }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </details>
 
-    <details open>
-      <summary>Ma'aser</summary>
-      <table>
-        <thead>
-          <tr>
-            <th>Description</th>
-            <th>Amount</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="(maaser, index) in maasers" :key="index">
-            <td>{{ maaser.description }}</td>
-            <td>{{ maaser.amount }}</td>
-            <td>{{ maaser.date.toLocaleDateString() }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </details>
+      <details open>
+        <summary>Ma'aser</summary>
+        <table>
+          <thead>
+            <tr>
+              <th>Description</th>
+              <th>Amount</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(maaser, index) in maasers" :key="index">
+              <td>{{ maaser.description }}</td>
+              <td>{{ maaser.amount }}</td>
+              <td>{{ maaser.date.toLocaleDateString() }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </details>
+    </article>
+    
+    <article>
+      <h3>Balance</h3>
+      <p>Total income: {{ totalIncome.toLocaleString() }}</p>
+      <p>Total ma'aser: {{ totalMaaser.toLocaleString() }}</p>
+      <p>Ma'aser due: {{ maaserDue.toLocaleString() }}</p>
+    </article>
     
   </main>
 </template>
