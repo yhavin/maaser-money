0556bff8a1b2e621dee50ff1ba3199beac330bd2
yhavin <y.havin@gmail.com>
2023-06-27 18:12:55 +1000

    Add Google Analytics

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
+import { getAnalytics, logEvent } from "firebase/analytics"
 
 
 const firebaseConfig = {
@@ -9,11 +10,13 @@ const firebaseConfig = {
   projectId: "ma-aser",
   storageBucket: "ma-aser.appspot.com",
   messagingSenderId: "530006556930",
-  appId: "1:530006556930:web:f2fbfd5abba2ad4d48821b"
+  appId: "1:530006556930:web:f2fbfd5abba2ad4d48821b",
+  measurementId: "G-7WKGLR1LH7"
 }
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
+const analytics = getAnalytics(app)
 
-export { db, auth }
+export { db, auth, analytics }
