7a365b68c23255ea288fb45e8458d60a8da53d2b
yhavin <y.havin@gmail.com>
2023-07-02 16:39:33 +1000

    Remove footer actions on splash page

--- src/pages/Splash.vue
+++ src/pages/Splash.vue
@@ -2,7 +2,6 @@
   import { ref } from "vue"
   import Login from '../components/Login.vue'
   import Register from '../components/Register.vue'
-  import FooterActions from "../components/FooterActions.vue"
   
   
   const showRegister = ref(false)
@@ -21,7 +20,6 @@
       <span><span v-if="!showRegister">Don't have an account? &nbsp;</span><a v-if="!showRegister" @click="toggleRegister">Sign up</a></span>
       <div v-if="showRegister"><Register /></div>
       <span><span v-if="showRegister">Already have an account? &nbsp;</span><a v-if="showRegister" @click="toggleRegister">Log in</a></span><br />
-      <FooterActions />
     </article>
   </main>
 </template>
