bd654c135ed43ca9b97697a169c9e5c720e329f0
yhavin <y.havin@gmail.com>
2023-07-05 19:24:51 +1000

    Add registration error messages

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,13 +5,13 @@
   import { useRouter } from "vue-router"
 
 
+  const router = useRouter()
+  
   const username = ref("")
   const password = ref("")
 
   const invalidLogin = ref()
   const errorMessage = ref()
-
-  const router = useRouter()
 
   const mapAuthErrors = {
     "auth/wrong-password": "The credentials you entered are incorrect",
@@ -29,7 +29,6 @@
     } catch (error) {
       errorMessage.value = mapAuthErrors[error.code]
       invalidLogin.value = true
-      console.log(errorMessage.value)
     }
   }
  
@@ -40,7 +39,7 @@
   <form @submit.prevent="login">
     <input v-model="username" type="text" placeholder="Email address" :aria-invalid="invalidLogin">
     <input v-model="password" type="password" placeholder="Password" :aria-invalid="invalidLogin">
-    <small class="error" v-if="errorMessage">{{ errorMessage }}</small>
+    <small class="error" v-if="invalidLogin">{{ errorMessage }}</small>
     <button>Continue</button>
   </form>
 </template>
