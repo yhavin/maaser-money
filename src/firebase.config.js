322a970d5fd685722de67d41d8bee0151d6efbf9
yhavin <y.havin@gmail.com>
2023-07-11 14:06:56 +1000

    Add cloud storage

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 import { getAnalytics } from "firebase/analytics"
+import { getStorage } from "firebase/storage"
 import { API_KEY } from "./env.js"
 
 
@@ -19,5 +20,6 @@ const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const auth = getAuth(app)
 const analytics = getAnalytics(app)
+const storage = getStorage(app)
 
-export { db, auth, analytics }
+export { db, auth, analytics, storage }
