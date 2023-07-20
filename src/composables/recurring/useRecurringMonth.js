17b724125647e3bc834cf39521a15b1f2a809b30
yhavin <y.havin@gmail.com>
2023-07-20 15:49:31 -0400

    Fix bug for creating first monthly recurring

--- src/composables/recurring/useRecurringMonth.js
+++ src/composables/recurring/useRecurringMonth.js
@@ -14,6 +14,30 @@ export const useRecurringMonth = async (schedule) => {
   const checkDateMs = schedule.endDate ? Math.min(endDateMs, currentDateMs) : currentDateMs
   console.log("Check until:", checkDateMs)
 
+  // TODO: Logic for creating first item (e.g., monthly schedule created on 13th to repeat on the 15th)
+  // A month hasn't passed yet but need that first item
+  // If list of repeated Item IDs is empty, then no first item yet
+  // How to tell if first item is needed?
+  // Today's day of month equals chosen day of month
+
+  console.log("Item IDs:", schedule.itemIds.length, "Date:", new Date().getDate() === schedule.dayOfMonth)
+  if (schedule.itemIds.length === 0 && new Date().getDate() === schedule.dayOfMonth) {
+    console.log("Creating first item...")
+    const firstItemDate = new Date(new Date().setHours(0, 0, 0))
+    schedule.prototype.date = firstItemDate
+
+    schedule.prototype.amount = schedule.prototype.conversion
+    ? await convertCurrency(schedule.prototype.baseAmount, schedule.prototype.baseCurrency, schedule.prototype.currency, firstItemDate)
+    : schedule.prototype.amount
+
+    const docRef = await addDoc(collectionRef, schedule.prototype)
+    console.log("Recurring item created in collection", schedule.type, "with ID:", docRef.id)
+    await updateDoc(docRef, { scheduleName: schedule.name})
+
+    await updateDoc(scheduleRef, { itemIds: arrayUnion(docRef.id) })
+    await updateDoc(scheduleRef, { lastRepeatedDate: firstItemDate })
+  }
+
   const itemsToCreate = Math.max(calculateElapsedMonths(lastRepeatedDateMs, checkDateMs), 0)
   console.log("Items:", itemsToCreate)
 
